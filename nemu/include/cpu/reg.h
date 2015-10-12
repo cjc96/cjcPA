baad24a9197a657196a60a1f157bebf1c286ca4d
tracer-ics2015 <tracer@njuics.org>
2015-10-12 16:52:00 +0800

    >  compile NEMU 141220000 cjc Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:52:00 up 41 min,  2 users,  load average: 0.29, 0.25, 0.18 a382b4c89efbb042f7f5ebaf82fac872623a5d8b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -16,86 +16,87 @@ enum { R_AL, R_CL, R_DL                                                         
 
 typedef struct {
 	union {
-	union {
-		uint32_t _32;
-		uint16_t _16;
-		struct{
-			uint8_t _8[2];
-		};
-	} gpr[8];
-
-	/* Do NOT change the order of the GPRs' definitions. */
-
-	/*uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi; */
-	struct {
-	union
-	{
-		uint32_t eax;
-		uint16_t ax;
-		struct
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			struct{
+				uint8_t _8[2];
+			};
+		} gpr[8];
+	
+		/* Do NOT change the order of the GPRs' definitions. */
+	
+		/*uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi; */
+		struct 
 		{
-			uint8_t al,ah;
+			union
+			{
+				uint32_t eax;
+				uint16_t ax;
+				struct
+				{
+					uint8_t al,ah;
+				};
+			};
+		
+			union
+			{
+				uint32_t ecx;
+				uint16_t cx;
+				struct
+				{
+					uint8_t cl,ch;
+				};
+			};
+		
+			union
+			{
+				uint32_t edx;
+				uint16_t dx;
+				struct
+				{
+					uint8_t dl,dh;
+				};
+			};
+		
+			union
+			{
+				uint32_t ebx;
+				uint16_t bx;
+				struct
+				{
+					uint8_t bl,bh;
+				};
+			};
+		
+			union
+			{
+				uint32_t esp;
+				uint16_t sp;
+			};
+		
+			union
+			{
+				uint32_t ebp;
+				uint16_t bp;
+			};
+			
+			union
+			{
+				uint32_t esi;
+				uint16_t si;
+			};
+		
+			union
+			{
+				uint32_t edi;
+				uint16_t di;
+			};
 		};
-	};
-
-	union
-	{
-		uint32_t ecx;
-		uint16_t cx;
-		struct
-		{
-			uint8_t cl,ch;
-		};
-	};
-
-	union
-	{
-		uint32_t edx;
-		uint16_t dx;
-		struct
-		{
-			uint8_t dl,dh;
-		};
-	};
-
-	union
-	{
-		uint32_t ebx;
-		uint16_t bx;
-		struct
-		{
-			uint8_t bl,bh;
-		};
-	};
-
-	union
-	{
-		uint32_t esp;
-		uint16_t sp;
-	};
-
-	union
-	{
-		uint32_t ebp;
-		uint16_t bp;
 	};
 	
-	union
-	{
-		uint32_t esi;
-		uint16_t si;
-	};
-
-	union
-	{
-		uint32_t edi;
-		uint16_t di;
-	};
-	};
-	};
-
 	swaddr_t eip;
-
+	
 } CPU_state;
 
 extern CPU_state cpu;
