d561de829aca15ff1d241d9a2bcaea9e41628cb6
tracer-ics2015 <tracer@njuics.org>
2015-10-21 21:15:19 +0800

    >  compile NEMU 14307130003 cjc Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:15:19 up  4:12,  2 users,  load average: 0.14, 0.30, 0.22 456f885bee1f30f71b726571398f11f7a586c2cd

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -90,6 +90,7 @@ static bool make_token(char *e) {
 
 				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s\n", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
+				getchar();
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array ``tokens''. For certain 
