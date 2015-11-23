7cac063faade4a69a231d22054f18c358706eae0
tracer-ics2015 <tracer@njuics.org>
2015-11-23 12:49:15 +0800

    >  compile NEMU 14307130003 cjc Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:49:15 up  1:13,  2 users,  load average: 0.12, 0.18, 0.16 b9e9344fffdb7646ef231bc9474c732d21478cfe

--- nemu/src/cpu/exec/control_transfer/jcc-template.h
+++ nemu/src/cpu/exec/control_transfer/jcc-template.h
@@ -3,28 +3,40 @@
 #define instr jcc
 
 static void do_execute () {
-	
-	switch (swaddr_read(cpu.eip,1))
+	int op_temp;
+	op_temp = swaddr_read(cpu.eip,1);
+	if (op_temp == 0x0f)
 	{
-		case 0x70 : if (cpu.OF) cpu.eip += op_src->val; break;
-		case 0x71 : if (!cpu.OF) cpu.eip += op_src->val; break;
-		case 0x72 : if (cpu.CF) cpu.eip += op_src->val; break;
-		case 0x73 : if (!cpu.CF) cpu.eip += op_src->val; break;
-		case 0x74 : if (cpu.ZF) cpu.eip += op_src->val; break;
-		case 0x75 : if (!cpu.ZF) cpu.eip += op_src->val; break;
-		case 0x76 : if ((cpu.CF) || (cpu.ZF)) cpu.eip += op_src->val; break;
-		case 0x77 : if ((!cpu.CF) && (!cpu.ZF)) cpu.eip += op_src->val; break;
-		case 0x78 : if (cpu.SF) cpu.eip += op_src->val; break;
-		case 0x79 : if (!cpu.SF) cpu.eip += op_src->val; break;
-		case 0x7a : if (cpu.PF) cpu.eip += op_src->val; break;
-		case 0x7b : if (!cpu.PF) cpu.eip += op_src->val; break;
-		case 0x7c : if (cpu.SF != cpu.OF) cpu.eip += op_src->val; break;
-		case 0x7d : if (cpu.SF == cpu.OF) cpu.eip += op_src->val; break;
-		case 0x7e : if ((cpu.ZF) || (cpu.SF != cpu.OF)) cpu.eip += op_src->val; break;
-		case 0x7f : if ((!cpu.ZF) && (cpu.SF == cpu.OF)) cpu.eip += op_src->val; break;
-		case 0xe3 : if (!cpu.cx) cpu.eip += op_src->val; break;
-		default : panic("Please implent me!");
+		assert(0);
 	}
+	else 
+		if (op_temp <= 0x7f && op_temp >= 0x70)
+		{
+			switch (op_temp)
+			{
+				case 0x70 : if (cpu.OF) cpu.eip += op_src->val; break;
+				case 0x71 : if (!cpu.OF) cpu.eip += op_src->val; break;
+				case 0x72 : if (cpu.CF) cpu.eip += op_src->val; break;
+				case 0x73 : if (!cpu.CF) cpu.eip += op_src->val; break;
+				case 0x74 : if (cpu.ZF) cpu.eip += op_src->val; break;
+				case 0x75 : if (!cpu.ZF) cpu.eip += op_src->val; break;
+				case 0x76 : if ((cpu.CF) || (cpu.ZF)) cpu.eip += op_src->val; break;
+				case 0x77 : if ((!cpu.CF) && (!cpu.ZF)) cpu.eip += op_src->val; break;
+				case 0x78 : if (cpu.SF) cpu.eip += op_src->val; break;
+				case 0x79 : if (!cpu.SF) cpu.eip += op_src->val; break;
+				case 0x7a : if (cpu.PF) cpu.eip += op_src->val; break;
+				case 0x7b : if (!cpu.PF) cpu.eip += op_src->val; break;
+				case 0x7c : if (cpu.SF != cpu.OF) cpu.eip += op_src->val; break;
+				case 0x7d : if (cpu.SF == cpu.OF) cpu.eip += op_src->val; break;
+				case 0x7e : if ((cpu.ZF) || (cpu.SF != cpu.OF)) cpu.eip += op_src->val; break;
+				case 0x7f : if ((!cpu.ZF) && (cpu.SF == cpu.OF)) cpu.eip += op_src->val; break;
+				case 0xe3 : if (!cpu.cx) cpu.eip += op_src->val; break;
+				default : panic("Please implent me!");
+			}
+		}
+		else
+			panic("Something wrong in JCC command!");
+		
 	print_asm_template1();
 }
 
