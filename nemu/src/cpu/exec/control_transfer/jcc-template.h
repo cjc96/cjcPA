816d7a22cabc68669a4b42b4b71349caade4895c
tracer-ics2015 <tracer@njuics.org>
2015-11-23 12:43:00 +0800

    >  compile NEMU 14307130003 cjc Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:43:00 up  1:07,  2 users,  load average: 0.17, 0.11, 0.14 227b5959bfd09ee049e8d9d0944456ce98050008

--- nemu/src/cpu/exec/control_transfer/jcc-template.h
+++ nemu/src/cpu/exec/control_transfer/jcc-template.h
@@ -6,7 +6,23 @@ static void do_execute () {
 	
 	switch (swaddr_read(cpu.eip,1))
 	{
+		case 0x70 : if (cpu.OF) cpu.eip += op_src->val; break;
+		case 0x71 : if (!cpu.OF) cpu.eip += op_src->val; break;
+		case 0x72 : if (cpu.CF) cpu.eip += op_src->val; break;
+		case 0x73 : if (!cpu.CF) cpu.eip += op_src->val; break;
 		case 0x74 : if (cpu.ZF) cpu.eip += op_src->val; break;
+		case 0x75 : if (!cpu.ZF) cpu.eip += op_src->val; break;
+		case 0x76 : if ((cpu.CF) || (cpu.ZF)) cpu.eip += op_src->val; break;
+		case 0x77 : if ((!cpu.CF) && (!cpu.ZF)) cpu.eip += op_src->val; break;
+		case 0x78 : if (cpu.SF) cpu.eip += op_src->val; break;
+		case 0x79 : if (!cpu.SF) cpu.eip += op_src->val; break;
+		case 0x7a : if (cpu.PF) cpu.eip += op_src->val; break;
+		case 0x7b : if (!cpu.PF) cpu.eip += op_src->val; break;
+		case 0x7c : if (cpu.SF != cpu.OF) cpu.eip += op_src->val; break;
+		case 0x7d : if (cpu.SF == cpu.OF) cpu.eip += op_src->val; break;
+		case 0x7e : if ((cpu.ZF) || (cpu.SF != cpu.OF)) cpu.eip += op_src->val; break;
+		case 0x7f : if ((!cpu.ZF) && (cpu.SF == cpu.OF)) cpu.eip += op_src->val; break;
+		case 0xe3 : if (!cpu.cx) cpu.eip += op_src->val; break;
 		default : panic("Please implent me!");
 	}
 	print_asm_template1();
