dfe8505ff058b1215b7420cdfb1629c202fa98cf
tracer-ics2015 <tracer@njuics.org>
2015-11-23 13:46:54 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  13:46:54 up 13 min,  2 users,  load average: 0.05, 0.19, 0.15 ba4ba803d25a997abb1b2afa8a1ea43dbd24c55a

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,27 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute () {
+	DATA_TYPE result = op_dest->val + op_src->val;
+	OPERAND_W(op_dest, result);
+
+	/* TODO: Update EFLAGS. */
+	//printf("%d %d\n",op_dest->val,op_src->val);
+	//panic("please implement me");
+	
+	int sin = 0,cin = 0;
+	set_eflags(op_dest->val,op_src->val,sin,cin);
+	
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
