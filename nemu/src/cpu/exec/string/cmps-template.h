070f67432cb98e377c8c4f9e1c921a692ec815ff
tracer-ics2015 <tracer@njuics.org>
2016-03-11 15:41:04 +0800

    >  compile NEMU 14307130003 cjc Linux cjc-OptiPlex-3020 3.16.0-57-generic #77~14.04.1-Ubuntu SMP Thu Dec 17 23:20:00 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:41:04 up  4:30,  2 users,  load average: 0.01, 0.03, 0.05 4cd4ed3c509d4921fd2baea6788a56aa8e5f0191

---
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -0,0 +1,48 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmps
+
+make_helper(concat(cmps_, SUFFIX)) {
+	
+	int incdec;
+	if (DATA_BYTE == 1)
+	{
+		int sin = 1, cin = 1;
+		int ain = swaddr_read(cpu.edi, 1), bin = swaddr_read(cpu.esi, 1);
+		set_eflags(ain, bin, sin, cin);
+	
+		if (!cpu.DF)
+			incdec = 1;
+		else
+			incdec = -1;
+	}
+	else if (DATA_BYTE == 2)
+	{
+		int sin = 1,cin = 1;
+		int ain = swaddr_read(cpu.edi, 1), bin = swaddr_read(cpu.esi, 1);
+		set_eflags(ain, bin, sin, cin);
+	
+		if (!cpu.DF)
+			incdec = 2;
+		else
+			incdec = -2;
+	}
+	else
+	{
+		int sin = 1,cin = 1;
+		int ain = swaddr_read(cpu.edi, 1), bin = swaddr_read(cpu.esi, 1);
+		set_eflags(ain, bin, sin, cin);
+	
+		if (!cpu.DF)
+			incdec = 4;
+		else
+			incdec = -4;
+	}
+	cpu.esi += incdec;
+	cpu.edi += incdec;
+	
+	print_asm_template1();
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
