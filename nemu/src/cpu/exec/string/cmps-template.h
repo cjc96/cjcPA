1a982d12f317f08aa136ce727772c9ba1c2f7f4b
tracer-ics2015 <tracer@njuics.org>
2016-03-12 21:13:26 +0800

    >  compile NEMU 14307130003 cjc Linux cjc-OptiPlex-3020 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:13:26 up 1 day,  4:54,  3 users,  load average: 0.03, 0.06, 0.12 da3b931b98cdd9516b87410976aaca72f1a9ee87

--- nemu/src/cpu/exec/string/cmps-template.h
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -20,7 +20,7 @@ make_helper(concat(cmps_, SUFFIX)) {
 	else if (DATA_BYTE == 2)
 	{
 		int sin = 1,cin = 1;
-		int ain = swaddr_read(cpu.edi, 1), bin = swaddr_read(cpu.esi, 1);
+		int ain = swaddr_read(cpu.edi, 2), bin = swaddr_read(cpu.esi, 2);
 		set_eflags(ain, bin, sin, cin);
 	
 		if (!cpu.DF)
@@ -31,7 +31,7 @@ make_helper(concat(cmps_, SUFFIX)) {
 	else
 	{
 		int sin = 1,cin = 1;
-		int ain = swaddr_read(cpu.edi, 1), bin = swaddr_read(cpu.esi, 1);
+		int ain = swaddr_read(cpu.edi, 4), bin = swaddr_read(cpu.esi, 4);
 		set_eflags(ain, bin, sin, cin);
 	
 		if (!cpu.DF)
