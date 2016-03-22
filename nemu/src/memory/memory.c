719919642726f141889126934635ed356dda662f
tracer-ics2015 <tracer@njuics.org>
2016-03-22 16:51:05 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:51:05 up  3:49,  2 users,  load average: 0.41, 0.18, 0.09 95f374496493cfecb0d5f336ed4608a463cef30

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -6,11 +6,29 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
+#ifdef CACHE
+	uint32_t musk = ~0u >> ((4 - len) << 3;
+	uint32_t tag = addr & 0xfffffe00 >> 9, group = addr & 0x000001c0 >> 6, offset = addr & 0x0000003f;
+	int i, start = group * 128, end = start + 128;
+	for (i = start; i < end; i++)
+	{
+		
+	}
+#endif
+
+#ifndef CACHE
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+#endif
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
+#ifdef CACHE
+
+#endif
+
+#ifndef CACHE
 	dram_write(addr, len, data);
+#endif
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
