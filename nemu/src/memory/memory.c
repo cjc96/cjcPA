e1137806f20b89bf99e613c8167a423c9489b5f0
tracer-ics2015 <tracer@njuics.org>
2016-04-05 11:12:04 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:12:04 up 14:57,  5 users,  load average: 0.22, 0.08, 0.06 150405c7c6d2ffdf6cc445118b3244cab8d3aaff

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -84,7 +84,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 		l1_cache[start_sp_temp].tag = tag_sp;
 		l1_cache[start_sp_temp].sign = 1;
 	}
-	//printf("Not hit.\nval = %x\taddr = %x\n",dram_read(addr, len) & musk, addr);
+	printf("Not hit.\nval = %x\taddr = %x\n",dram_read(addr, len) & musk, addr);
 	if (addr == 0x100019) printf("haha\n");
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 #endif
