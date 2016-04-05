87962843e110eaed35da40b4d9e189510fd59169
tracer-ics2015 <tracer@njuics.org>
2016-04-05 11:03:27 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:03:27 up 14:48,  5 users,  load average: 0.02, 0.04, 0.06 904daf7bb61ed68429092a0039eaeea150cc1c79

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -65,9 +65,9 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	}
 	
 	uint32_t temp = rand_temp() % 128 + start, temp_set = addr - offset;
-	if (temp == 259)
-		printf("%x %zu\n", addr, len);
-		
+	//if (temp == 259)
+		//printf("%x %zu\n", addr, len);
+	printf("1\n");
 	for (i = 0; i < 16; i++)
 	{
 		l1_cache[temp].data_d[i] = dram_read(temp_set + i * 4, 4);
