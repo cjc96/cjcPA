86ca8c97156071b3e93f1b37e0e1d8b9e6f1e68c
tracer-ics2015 <tracer@njuics.org>
2016-04-12 10:14:49 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  10:14:49 up  1:29,  2 users,  load average: 0.24, 0.14, 0.08 29634598dad99b016dfc553d38517851360a80a

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,6 +2,7 @@
 #define __REG_H__
 
 #include "common.h"
+#include "../../lib-common/x86-inc/cpu.h"
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
@@ -155,6 +156,10 @@ typedef struct {
 			};
 		};
 	};
+	
+	uint32_t GDTR, SS, DS, ES, CS;
+	
+	CR0 cr0;
 	
 } CPU_state;
 
