bfee47d408a7369aef67b857d1e44b0560f37c7a
tracer-ics2015 <tracer@njuics.org>
2016-04-15 19:14:14 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:14:14 up  2:18,  3 users,  load average: 0.18, 0.17, 0.10 b86c6644b9b6ba38c19f06e8572c49e62ef1fb4c

--- nemu/src/cpu/exec/control_transfer/jmpsp.c
+++ nemu/src/cpu/exec/control_transfer/jmpsp.c
@@ -3,14 +3,14 @@
 
 make_helper(jmpsp) 
 {
-	int16_t temp1 = swaddr_read(cpu.eip + 1, 2);
-	int32_t temp2 = swaddr_read(cpu.eip + 3, 4);
-	cpu.CS = temp1;
-	cpu.eip = temp2;	
+	uint32_t temp1 = swaddr_read(cpu.eip + 1, 4);
+	uint16_t temp2 = swaddr_read(cpu.eip + 3, 2);
+	cpu.CS = temp2;
+	cpu.eip = temp1;	
 
 	extern int do_jmpnear;
 	do_jmpnear = 1;
 
-	print_asm("ljmp $0x%x, $0x%x", temp1, temp2);
+	print_asm("ljmp $0x%x, $0x%x", temp2, temp1);
 	return 1;
 }
