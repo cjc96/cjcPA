8e9281ecb34a1eee5178d2340b869d102c136323
tracer-ics2015 <tracer@njuics.org>
2016-04-17 12:02:35 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:02:35 up  5:25,  3 users,  load average: 0.32, 0.15, 0.10 139de37783a5b46f202348768272bad61f59e0ed

--- nemu/src/cpu/exec/data-mov/movseg-template.h
+++ nemu/src/cpu/exec/data-mov/movseg-template.h
@@ -6,11 +6,11 @@ static void do_execute () {
 	uint32_t rmcode = (swaddr_read(cpu.eip + 1, 1) >> 3) & 0x7;
 	
 	if (rmcode == 2)
-		cpu.SS = op_src->addr;
+		cpu.SS = op_dest->val;
 	else if (rmcode == 3)
-		cpu.DS = op_src->addr;
+		cpu.DS = op_dest->val;
 	else if (rmcode == 0)
-		cpu.ES = op_src->addr;
+		cpu.ES = op_dest->val;
 	else
 		assert(0);
 	
