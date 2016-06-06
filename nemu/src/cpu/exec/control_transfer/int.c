4e9bd886de82f08c21ae83622500c1ce3ae192a7
tracer-ics2015 <tracer@njuics.org>
2016-06-06 14:35:24 +0800

    >  compile NEMU 14307130003 caojingchen Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:35:24 up  5:22,  2 users,  load average: 0.01, 0.06, 0.11 46af5999d84c8c53ee035c9f26b898daed871483

--- nemu/src/cpu/exec/control_transfer/int.c
+++ nemu/src/cpu/exec/control_transfer/int.c
@@ -1,10 +1,12 @@
 #include "cpu/exec/helper.h"
+#include "cpu/exec/interrupt.h"
 
 /* for instruction encoding overloading */
 
 make_helper(ret_i_b)
 {
-	print_asm("ret");
+	print_asm_template1();
+	raise_intr(op_src->val);
 	
 	return 1;
 }
