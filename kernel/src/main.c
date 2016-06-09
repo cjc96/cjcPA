d57dd215cc4341b82f8cb270a9217ab12488f210
tracer-ics2015 <tracer@njuics.org>
2016-06-10 00:34:41 +0800

    >  compile game 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:34:41 up  4:23,  2 users,  load average: 0.06, 0.18, 0.20 decf729df24d19eee634d2da177b815fc25433ce

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -85,7 +85,6 @@ void init_cond() {
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
 	 */
-	 Log("123");
 	video_mapping_read_test();
 
 	/* Clear the test data we just written in the video memory. */
