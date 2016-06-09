43f5c4475207d8164af1275eb632ed97cf965ba0
tracer-ics2015 <tracer@njuics.org>
2016-06-09 23:38:30 +0800

    >  compile NEMU 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:38:30 up  3:26,  3 users,  load average: 0.56, 0.25, 0.22 447cf228853245f913a975c65da379d503c1d9e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -211,7 +211,7 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 #endif
 	if (cpu.cr0.paging && cpu.cr0.protect_enable)
 	{
-		if ((addr & 0xfff) + len > 4095) {
+		if ((addr & 0xfff) + len >= 4095) {
 			/* this is a special case, you can handle it later. */
 			//assert(0);
 			uint32_t temp_ans = 0;
@@ -236,7 +236,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 #endif
 	if (cpu.cr0.paging && cpu.cr0.protect_enable)
 	{
-		if ((addr & 0xfff) + len > 4095) {
+		if ((addr & 0xfff) + len >= 4095) {
 		
 			/* this is a special case, you can handle it later. */
 			//assert(0);
