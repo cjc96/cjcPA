b28009a04300c50d719d30ef3cb7d4cbd792de23
tracer-ics2015 <tracer@njuics.org>
2016-06-14 11:06:51 +0800

    >  compile NEMU 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  11:06:51 up  1:00,  3 users,  load average: 0.23, 0.11, 0.14 f34c4c9d908a47918d3d4673cd2abd9bd52fcfe4

--- Makefile
+++ Makefile
@@ -53,7 +53,7 @@ clean: clean-cpp
 
 ##### some convinient rules #####
 
-USERPROG := obj/testcase/movzx_high
+USERPROG := obj/testcase/LLneg
 #USERPROG := $(game_BIN)
 # ENTRY := $(USERPROG)
  ENTRY := $(kernel_BIN)
--- nemu/src/cpu/exec/arith/neg-template.h
+++ nemu/src/cpu/exec/arith/neg-template.h
@@ -5,6 +5,14 @@
 static void do_execute() {
 	DATA_TYPE result = -op_src->val;
 	OPERAND_W(op_src, result);
+	if (op_src == 0)
+	{
+		cpu.CF = 0;
+	}
+	else
+	{
+		cpu.CF = 1;
+	}
 
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
