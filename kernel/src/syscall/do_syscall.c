d8eff2d5e8cf65fb94ecdfc35ca1816ad965f3d9
tracer-ics2015 <tracer@njuics.org>
2016-06-14 13:51:17 +0800

    >  compile kernel 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:51:17 up  3:04,  3 users,  load average: 0.14, 0.05, 0.10 f1392bb3f96daf3b6749872775c36b2f9e2f692

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -6,11 +6,11 @@ void add_irq_handle(int, void (*)(void));
 void mm_brk(uint32_t);
 void serial_printc(char);
 
-int fs_open(const char *pathname, int flags);
-int fs_read(int fd, void *buf, int len);
-int fs_write(int fd, const void *buf, int len);
-off_t fs_lseek(int fd, int offset, int whence);
-int fs_close(int fd);
+extern int fs_open(const char *pathname, int flags);
+extern int fs_read(int fd, void *buf, int len);
+extern int fs_write(int fd, const void *buf, int len);
+extern off_t fs_lseek(int fd, int offset, int whence);
+extern int fs_close(int fd);
 
 static void sys_brk(TrapFrame *tf) {
 #ifdef IA32_PAGE
