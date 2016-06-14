841e01fecbfe3af13501a6baba5aa534cd7859d4
tracer-ics2015 <tracer@njuics.org>
2016-06-14 22:03:11 +0800

    >  compile kernel 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:03:11 up  9:25,  2 users,  load average: 0.09, 0.08, 0.10 2e17e10a2cf5e75813cb5dddf199e4f917d1fac

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -1,101 +1,99 @@
-#include <string.h>
 #include "common.h"
+#include <string.h>
 
 typedef struct {
-	char *name;
-	uint32_t size;
-	uint32_t disk_offset;
+    char *name;
+    uint32_t size;
+    uint32_t disk_offset;
 } file_info;
 
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 
 /* This is the information about all files in disk. */
 static const file_info file_table[] = {
-	{"1.rpg", 188864, 1048576}, {"2.rpg", 188864, 1237440},
-	{"3.rpg", 188864, 1426304}, {"4.rpg", 188864, 1615168},
-	{"5.rpg", 188864, 1804032}, {"abc.mkf", 1022564, 1992896},
-	{"ball.mkf", 134704, 3015460}, {"data.mkf", 66418, 3150164},
-	{"desc.dat", 16027, 3216582}, {"fbp.mkf", 1128064, 3232609},
-	{"fire.mkf", 834728, 4360673}, {"f.mkf", 186966, 5195401},
-	{"gop.mkf", 11530322, 5382367}, {"map.mkf", 1496578, 16912689},
-	{"mgo.mkf", 1577442, 18409267}, {"m.msg", 188232, 19986709},
-	{"mus.mkf", 331284, 20174941}, {"pat.mkf", 8488, 20506225},
-	{"rgm.mkf", 453202, 20514713}, {"rng.mkf", 4546074, 20967915},
-	{"sss.mkf", 557004, 25513989}, {"voc.mkf", 1997044, 26070993},
-	{"wor16.asc", 5374, 28068037}, {"wor16.fon", 82306, 28073411},
-	{"word.dat", 5650, 28155717},
+    {"1.rpg", 188864, 1048576}, {"2.rpg", 188864, 1237440},
+    {"3.rpg", 188864, 1426304}, {"4.rpg", 188864, 1615168},
+    {"5.rpg", 188864, 1804032}, {"abc.mkf", 1022564, 1992896},
+    {"ball.mkf", 134704, 3015460}, {"data.mkf", 66418, 3150164},
+    {"desc.dat", 16027, 3216582}, {"fbp.mkf", 1128064, 3232609},
+    {"fire.mkf", 834728, 4360673}, {"f.mkf", 186966, 5195401},
+    {"gop.mkf", 11530322, 5382367}, {"map.mkf", 1496578, 16912689},
+    {"mgo.mkf", 1577442, 18409267}, {"m.msg", 188232, 19986709},
+    {"mus.mkf", 331284, 20174941}, {"pat.mkf", 8488, 20506225},
+    {"rgm.mkf", 453202, 20514713}, {"rng.mkf", 4546074, 20967915},
+    {"sss.mkf", 557004, 25513989}, {"voc.mkf", 1997044, 26070993},
+    {"wor16.asc", 5374, 28068037}, {"wor16.fon", 82306, 28073411},
+    {"word.dat", 5650, 28155717},
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
 void ide_read(uint8_t *, uint32_t, uint32_t);
-void ide_write(uint8_t *, uint32_t, uint32_t);
-
-/* TODO: implement a simplified file system here. */
+void ide_write(const uint8_t *, uint32_t, uint32_t);
 
 struct {
-	bool opened;
-	uint32_t offset;
-} Fstate[NR_FILES + 3];
+    bool opened;
+    uint32_t offset;
+} f_state[NR_FILES + 3];
 
 int fs_open(const char *pathname, int flags) {
 	int i;
-	for(i = 0; i < NR_FILES; i++) {
-		if(strcmp(pathname,file_table[i].name) == 0) {
-			Fstate[i + 3].opened = true;
-			Fstate[i + 3].offset = 0;
-			printk("syscall: open(\"%s\", %d) = %d\n", pathname, flags, i);
-			return i + 3;
-		}
-	}
-	nemu_assert(0);
-    return 0;
+    for (i = 0; i < NR_FILES; i++) {
+        if (strcmp(file_table[i].name, pathname) == 0) {
+            int fd = i + 3;
+            f_state[fd].opened = true;
+            f_state[fd].offset = 0;
+            return fd;
+        }
+    }
+    panic("file not found");
+    return -1;
 }
 
-int fs_read(int fd, void *buf, int len) {
-	//int len_before = len;
-	if(Fstate[fd].offset + len > file_table[fd - 3].size) 
-		len = file_table[fd - 3].size - Fstate[fd].offset;
-	ide_read(buf, file_table[fd - 3].disk_offset + Fstate[fd].offset, len);
-	Fstate[fd].offset += len;
-	//printk("syscall: read( %d, \"%s\", %d) = %d\n", fd, buf, len_before, len);
-	return len;
+int fs_read(int fd, void *buf, size_t len) {
+    if (fd < 3) return -1;
+    assert(fd < NR_FILES + 3);
+    assert(f_state[fd].opened);
+    int max_len = file_table[fd - 3].size - f_state[fd].offset;
+    assert(max_len >= 0);
+    if (max_len < len) len = max_len;
+    ide_read(buf, file_table[fd - 3].disk_offset + f_state[fd].offset, len);
+    f_state[fd].offset += len;
+    return len;
 }
 
-int fs_write(int fd, void *buf, int len) {
-	//int len_before = len;
-	if(Fstate[fd].offset + len > file_table[fd - 3].size)
-		len = file_table[fd - 3].size - Fstate[fd].offset;
-	ide_write(buf, file_table[fd - 3].disk_offset + Fstate[fd].offset, len);
-	Fstate[fd].offset += len;
-	//printk("syscall: write( %d, \"%s\", %d) = %d\n", fd, buf, len_before, len);
-	return len;
+int fs_write(int fd, const void *buf, size_t len) {
+    if (fd < 3) return -1;
+    assert(fd < NR_FILES + 3);
+    assert(f_state[fd].opened);
+    int max_len = file_table[fd - 3].size - f_state[fd].offset;
+    assert(max_len >= 0);
+    if (max_len < len) len = max_len;
+    ide_write(buf, file_table[fd - 3].disk_offset + f_state[fd].offset, len);
+    f_state[fd].offset += len;
+    return len;
 }
-	
-int fs_lseek(int fd, int offset, int whence) {
-	unsigned new_offset = 0;
-	if(whence == SEEK_SET) new_offset = offset;
-	else if(whence == SEEK_CUR) new_offset = file_table[fd - 3].disk_offset + offset;
-	else if(whence == SEEK_END) new_offset = file_table[fd - 3].size + offset;
-	else panic("unknown whence %d", whence);
-	Fstate[fd].offset = new_offset;
-	return new_offset;
+
+off_t fs_lseek(int fd, off_t offset, int whence) {
+    if (fd < 3) return -1;
+    assert(fd < NR_FILES + 3);
+    assert(f_state[fd].opened);
+    switch (whence) {
+        case SEEK_SET: f_state[fd].offset = 0; break;
+        case SEEK_CUR: break;
+        case SEEK_END: f_state[fd].offset = file_table[fd - 3].size; break;
+        default: assert(0);
+    }
+    f_state[fd].offset += offset;
+    assert(f_state[fd].offset >= 0);
+    assert(f_state[fd].offset <= file_table[fd - 3].size);
+    return f_state[fd].offset;
 }
 
 int fs_close(int fd) {
-	Fstate[fd].opened = false;
-	printk("syscall: close(%d) [fn = %s]\n", fd, file_table[fd - 3].name);
-	return 0;
+    assert(f_state[fd].opened);
+    f_state[fd].opened = false;
+    return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
+    
--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -4,87 +4,83 @@
 
 void add_irq_handle(int, void (*)(void));
 void mm_brk(uint32_t);
+void serial_printc(char);
 
-extern int fs_open(const char *pathname, int flags);
-extern int fs_read(int fd, void *buf, int len);
-extern int fs_write(int fd, void *buf, int len);
-extern int fs_lseek(int fd, int offset, int whence);
-extern int fs_close(int fd);
+int fs_open(const char *pathname, int flags);
+int fs_read(int fd, void *buf, size_t len);
+int fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
 
 static void sys_brk(TrapFrame *tf) {
 #ifdef IA32_PAGE
-	mm_brk(tf->ebx);
+    mm_brk(tf->ebx);
 #endif
-	tf->eax = 0;
+    tf->eax = 0;
 }
-void serial_printc(char ch);
 
-static void sys_write(TrapFrame *tf) {
-    int i;
-	int fd = tf->ebx;
-	uint32_t buf = tf->ecx;
-	size_t len = tf->edx;
-	
-	if(fd == 1 || fd == 2) 
-	{
-		//asm volatile (".byte 0xd6" : : "a"(2), "c"(buf), "d"(len));
-        for(i = 0; i < len; i++) {
-            char *output = (char *)(buf + i);
-            serial_printc(*output);
-        }
-        if(len == 0) tf->eax = (uint32_t) -1;
-        else tf->eax = 0;
-	} else {
-        tf->eax = fs_write(tf->ebx, (void *)tf->ecx, tf->edx);
+static inline void sys_write(TrapFrame *tf) {
+    uint32_t fd = tf->ebx;
+    char *buf = (char *)tf->ecx;
+    uint32_t len = tf->edx;
+    uint32_t result = 0;
+    if (fd == 1 || fd == 2) {
+        while (len--) serial_printc(*(buf++));
+        result = tf->edx;
+    } else {
+        result = fs_write(fd, buf, len);
     }
-	/*if((fd == 1 || fd == 2) && (len == 0)) tf->eax = -1;
-	else tf->eax = 0;*/
+    tf->eax = result;
 }
 
 static void sys_open(TrapFrame *tf) {
-    tf->eax = fs_open((void *) tf->ebx, tf->ecx);
+    const char *pathname = (const char *)tf->ebx;
+    int flags = tf->ecx;
+    tf->eax = fs_open(pathname, flags);
 }
 
 static void sys_close(TrapFrame *tf) {
-    tf->eax = fs_close(tf->ebx);
+    int fd = tf->ebx;
+    tf->eax = fs_close(fd);
 }
 
 static void sys_read(TrapFrame *tf) {
-    tf->eax = fs_read(tf->ebx, (void *) tf->ecx, tf->edx);
+    int fd = tf->ebx;
+    char *buf = (char *) tf->ecx;
+    size_t len = tf->edx;
+    tf->eax = fs_read(fd, buf, len);
 }
 
 static void sys_lseek(TrapFrame *tf) {
-    tf->eax = fs_lseek(tf->ebx, tf->ecx, tf->edx);
+    int fd = tf->ebx;
+    off_t offset = tf->ecx;
+    int whence = tf->edx;
+    tf->eax = fs_lseek(fd, offset, whence);
 }
 
 void do_syscall(TrapFrame *tf) {
-	switch(tf->eax) {
-		/* The ``add_irq_handle'' system call is artificial. We use it to 
-		 * let user program register its interrupt handlers. But this is 
-		 * very dangerous in a real operating system. Therefore such a 
-		 * system call never exists in GNU/Linux.
-		 */
-		case 0: 
-			cli();
-			add_irq_handle(tf->ebx, (void*)tf->ecx);
-			sti();
-			break;
+    switch(tf->eax) {
+        /* The ``add_irq_handle'' system call is artificial. We use it to 
+         * let user program register its interrupt handlers. But this is 
+         * very dangerous in a real operating system. Therefore such a 
+         * system call never exists in GNU/Linux.
+         */
+        case 0: 
+            cli();
+            add_irq_handle(tf->ebx, (void*)tf->ecx);
+            sti();
+            break;
 
-		case SYS_brk: sys_brk(tf); break;
-		
-		case SYS_write: sys_write(tf); break;
-		
+        case SYS_brk: sys_brk(tf); break;
+
+        /* Add more system calls. */
         case SYS_open: sys_open(tf); break;
-
         case SYS_read: sys_read(tf); break;
-
+        case SYS_write: sys_write(tf); break;
         case SYS_lseek: sys_lseek(tf); break;
-
         case SYS_close: sys_close(tf); break;
 
-		/* TODO: Add more system calls. */
-
-		default: panic("Unhandled system call: id = %d", tf->eax);
-	}
+        default: panic("Unhandled system call: id = %d", tf->eax);
+    }
 }
 
