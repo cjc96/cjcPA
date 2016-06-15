028d59fc8df51445b683ee6fdbae950f6fe62a7d
tracer-ics2015 <tracer@njuics.org>
2016-06-15 11:08:24 +0800

    >  run 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  11:08:24 up  1:25,  2 users,  load average: 0.17, 0.13, 0.22 ad2badcbbb89fdfc9830edf3dda09b573a69f24

--- testcase/src/LLdiv.c
+++ testcase/src/LLdiv.c
@@ -23,6 +23,68 @@ SOFTWARE.
 
 */
 
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
+
+#include "trap.h"
+
+int __attribute__((__noinline__))
+syscall(int id, ...) {
+	int ret;
+	int *args = &id;
+	asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
+	return ret;
+}
+
+int read(int fd, char *buf, int len) {
+	nemu_assert(0);
+	return 0;
+}
+
+int write(int fd, char *buf, int len) {
+	return syscall(__NR_write, fd, buf, len); 
+}
+
+off_t lseek(int fd, off_t offset, int whence) {
+	nemu_assert(0);
+	return 0;
+}
+
+void *sbrk(int incr) {
+	extern char end;		/* Defined by the linker */
+	static char *heap_end;
+	char *prev_heap_end;
+
+	if (heap_end == 0) {
+		heap_end = &end;
+	}
+	prev_heap_end = heap_end;
+
+	if( syscall(SYS_brk, heap_end + incr) == 0) {
+		heap_end += incr;
+	}
+
+	return prev_heap_end;
+}
+
+int close(int fd) {
+	nemu_assert(0);
+	return 0;
+}
+
+int fstat(int fd, struct stat *buf) {
+//	buf->st_mode = S_IFCHR;
+	return 0;
+}
+
+int isatty(int fd) {
+	nemu_assert(0);
+	return 0;
+}
+
 #include "trap.h"
 
 typedef unsigned long long ULL;
@@ -336,6 +398,7 @@ void test_ ## TYPE() \
         sample_q = sample[samplep++]; \
         sample_r = sample[samplep++]; \
         /*printf("0x%llx, 0x%llx, /" "* 0x%llx div 0x%llx *" "/\n", q, r, a, b);*/ \
+        printf("%d\n", i);\
         nemu_assert(sample_q == q); \
         nemu_assert(sample_r == r); \
     } \
@@ -368,6 +431,7 @@ int naive_memcmp(void *p1, void *p2, int n)
 
 int main()
 {
+	set_bp();
     test_LL();
     test_ULL();
     /*int i;
