a8e8c5727b224316bfab93beaab8b03cdcff33c8
tracer-ics2015 <tracer@njuics.org>
2016-06-15 11:00:20 +0800

    >  run 14307130003 cjc96 Linux vm2 4.2.0-36-generic #42~14.04.1-Ubuntu SMP Fri May 13 17:27:22 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  11:00:20 up  1:17,  2 users,  load average: 0.02, 0.13, 0.29 34ea76a1edae828917883bdceac364c5484d1b00

--- testcase/src/LLdiv.c
+++ testcase/src/LLdiv.c
@@ -22,8 +22,66 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
-
+#include <stdio.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
 #include "trap.h"
+
+
+int __attribute__((__noinline__))
+syscall(int id, ...) {
+	int ret;
+	int *args = &id;
+	asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
+	return ret;
+}
+
+int read(int fd, char *buf, int len) {
+	nemu_assert(0);
+	return 0;
+}
+
+int write(int fd, char *buf, int len) {
+	return syscall(__NR_write, fd, buf, len); 
+}
+
+off_t lseek(int fd, off_t offset, int whence) {
+	nemu_assert(0);
+	return 0;
+}
+
+void *sbrk(int incr) {
+	extern char end;		/* Defined by the linker */
+	static char *heap_end;
+	char *prev_heap_end;
+
+	if (heap_end == 0) {
+		heap_end = &end;
+	}
+	prev_heap_end = heap_end;
+
+	if( syscall(SYS_brk, heap_end + incr) == 0) {
+		heap_end += incr;
+	}
+
+	return prev_heap_end;
+}
+
+int close(int fd) {
+	nemu_assert(0);
+	return 0;
+}
+
+int fstat(int fd, struct stat *buf) {
+//	buf->st_mode = S_IFCHR;
+	return 0;
+}
+
+int isatty(int fd) {
+	nemu_assert(0);
+	return 0;
+}
 
 typedef unsigned long long ULL;
 typedef long long LL;
@@ -335,7 +393,7 @@ void test_ ## TYPE() \
         TYPE ## div ## TYPE (a, b, &q, &r); \
         sample_q = sample[samplep++]; \
         sample_r = sample[samplep++]; \
-        /*printf("0x%llx, 0x%llx, /" "* 0x%llx div 0x%llx *" "/\n", q, r, a, b);*/ \
+        printf("0x%llx, 0x%llx, /" "* 0x%llx div 0x%llx *" "/\n", q, r, a, b); \
         nemu_assert(sample_q == q); \
         nemu_assert(sample_r == r); \
     } \
